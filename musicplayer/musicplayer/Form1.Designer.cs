353426a6c21e5477a37793e49a41580f2472a1cd
inane-phe <127912578+inane-phe@users.noreply.github.com>
2023-10-17 17:01:07 +0800

    Add files via upload

---
+++ musicplayer/musicplayer/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace musicplayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Do = new System.Windows.Forms.Button();
+            this.Re = new System.Windows.Forms.Button();
+            this.Mi = new System.Windows.Forms.Button();
+            this.Fa = new System.Windows.Forms.Button();
+            this.So = new System.Windows.Forms.Button();
+            this.La = new System.Windows.Forms.Button();
+            this.Si = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Do
+            // 
+            this.Do.Location = new System.Drawing.Point(50, 219);
+            this.Do.Name = "Do";
+            this.Do.Size = new System.Drawing.Size(75, 23);
+            this.Do.TabIndex = 0;
+            this.Do.Text = "Do";
+            this.Do.UseVisualStyleBackColor = true;
+            this.Do.Click += new System.EventHandler(this.Do_Click);
+            this.Do.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Do_KeyPress);
+            // 
+            // Re
+            // 
+            this.Re.Location = new System.Drawing.Point(125, 219);
+            this.Re.Name = "Re";
+            this.Re.Size = new System.Drawing.Size(75, 23);
+            this.Re.TabIndex = 1;
+            this.Re.Text = "Re";
+            this.Re.UseVisualStyleBackColor = true;
+            this.Re.Click += new System.EventHandler(this.Re_Click);
+            // 
+            // Mi
+            // 
+            this.Mi.Location = new System.Drawing.Point(200, 219);
+            this.Mi.Name = "Mi";
+            this.Mi.Size = new System.Drawing.Size(75, 23);
+            this.Mi.TabIndex = 2;
+            this.Mi.Text = "Mi";
+            this.Mi.UseVisualStyleBackColor = true;
+            this.Mi.Click += new System.EventHandler(this.Mi_Click);
+            // 
+            // Fa
+            // 
+            this.Fa.Location = new System.Drawing.Point(275, 219);
+            this.Fa.Name = "Fa";
+            this.Fa.Size = new System.Drawing.Size(75, 23);
+            this.Fa.TabIndex = 3;
+            this.Fa.Text = "Fa";
+            this.Fa.UseVisualStyleBackColor = true;
+            this.Fa.Click += new System.EventHandler(this.Fa_Click);
+            // 
+            // So
+            // 
+            this.So.Location = new System.Drawing.Point(350, 219);
+            this.So.Name = "So";
+            this.So.Size = new System.Drawing.Size(75, 23);
+            this.So.TabIndex = 4;
+            this.So.Text = "So";
+            this.So.UseVisualStyleBackColor = true;
+            this.So.Click += new System.EventHandler(this.So_Click);
+            // 
+            // La
+            // 
+            this.La.Location = new System.Drawing.Point(425, 219);
+            this.La.Name = "La";
+            this.La.Size = new System.Drawing.Size(75, 23);
+            this.La.TabIndex = 5;
+            this.La.Text = "La";
+            this.La.UseVisualStyleBackColor = true;
+            this.La.Click += new System.EventHandler(this.La_Click);
+            // 
+            // Si
+            // 
+            this.Si.Location = new System.Drawing.Point(500, 219);
+            this.Si.Name = "Si";
+            this.Si.Size = new System.Drawing.Size(75, 23);
+            this.Si.TabIndex = 6;
+            this.Si.Text = "Si";
+            this.Si.UseVisualStyleBackColor = true;
+            this.Si.Click += new System.EventHandler(this.Si_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 294);
+            this.Controls.Add(this.Si);
+            this.Controls.Add(this.La);
+            this.Controls.Add(this.So);
+            this.Controls.Add(this.Fa);
+            this.Controls.Add(this.Mi);
+            this.Controls.Add(this.Re);
+            this.Controls.Add(this.Do);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Do;
+        private System.Windows.Forms.Button Re;
+        private System.Windows.Forms.Button Mi;
+        private System.Windows.Forms.Button Fa;
+        private System.Windows.Forms.Button So;
+        private System.Windows.Forms.Button La;
+        private System.Windows.Forms.Button Si;
+    }
+}
+
---
+++ musicplayer/musicplayer/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace musicplayer
+{
+    public partial class Form1 : Form
+    {
+        int minX = 240, maxX = 800, minY = 30, maxY = 380;
+        int count = 0;
+        int[] fq = { 440, 495, 550, 587, 660, 733, 825, 895 }; 
+        int duration = 600;
+        int[] arrbee = new int[9] { 5, 3, 3, 4, 2, 2, 1,2,3 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            
+
+            this.ClientSize = new System.Drawing.Size(860, 660); 
+            Do.Location = new Point(240, 390);
+            Re.Location = new Point(320, 390);
+            Mi.Location = new Point(400, 390);
+            Fa.Location = new Point(480, 390);
+            So.Location = new Point(560, 390);
+            La.Location = new Point(640, 390);
+            Si.Location = new Point(725, 390);
+            this.KeyPreview = true;
+            paintTable(arrbee,9);
+
+
+        }
+
+        private void Do_Click(object sender, EventArgs e)
+        {
+            Console.Beep(fq[0], duration);//發出聲音
+
+        }
+
+        private void Re_Click(object sender, EventArgs e)
+        {
+            Console.Beep(fq[1], duration);
+            count += 1;
+        }
+
+        private void Mi_Click(object sender, EventArgs e)
+        {
+            Console.Beep(fq[2], duration);
+            count += 1;
+        }
+
+        private void Fa_Click(object sender, EventArgs e)
+        {
+            Console.Beep(fq[3], duration);
+            count += 1;
+        }
+
+        private void So_Click(object sender, EventArgs e)
+        {
+            Console.Beep(fq[4], duration);
+            count += 1;
+        }
+
+        private void La_Click(object sender, EventArgs e)
+        {
+            Console.Beep(fq[5], duration);
+            count += 1;
+        }
+
+        private void Si_Click(object sender, EventArgs e)
+        {
+            Console.Beep(fq[6], duration);
+            count += 1;
+        }
+
+
+        void paintTable(int[] arr, int max){
+            Graphics g = this.CreateGraphics();
+
+            g.Clear(Color.White);
+
+
+            for (int i = 0; i <= 7; i++)
+            {
+                g.DrawLine(new Pen(Color.Black), new Point(minX, minY + ((380 - 30) / 7 * i)), new Point(maxX, minY + (380 - 30) / 7 * i));
+                g.DrawLine(new Pen(Color.Black), new Point(minX + ((800 - 240) / 7 * i), minY), new Point(minX + ((800 - 240) / 7 * i), maxY));
+            }
+
+            /*for (int i = count; i <=count+6; j++){
+                g.FillRectangle(new SolidBrush(Color.Black), 240+80*(Array.IndexOf(step,sheet[i])), 30+50*(6-i) , 80, 50);
+             }*/
+            for (int i=6,j=count; i>=0 ; i--){
+                if (j > max - 1)
+                {
+                }
+                else
+                {
+                    g.FillRectangle(new SolidBrush(Color.Black), minX + (800 - 240) / 7 * (arr[j++] - 1), minY + (380 - 30) / 7 * i, (800 - 240) / 7, (380 - 30) / 7);
+                }
+            }           
+        }
+
+        private void Do_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.D1)
+            {
+                Do_Click(sender, e);
+            } 
+
+            if (e.KeyChar == (char)Keys.D2)
+            {
+                Re_Click(sender, e);
+            }
+            if (e.KeyChar == (char)Keys.D3)
+            {
+                Mi_Click(sender, e);
+            }
+            if (e.KeyChar == (char)Keys.D4)
+            {
+                Fa_Click(sender, e);
+            }
+            if (e.KeyChar == (char)Keys.D5)
+            {
+                So_Click(sender, e);
+            }
+            if (e.KeyChar == (char)Keys.D6)
+            {
+                La_Click(sender, e);
+            }
+            if (e.KeyChar == (char)Keys.D7)
+            {
+                Si_Click(sender, e);
+            }
+        }
+    }
+}
